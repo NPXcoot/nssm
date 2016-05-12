3e2555ba6843e8cbd0369bd5dd01f93080ad74d2
npx <npx_npx@yahoo.it>
2016-05-12 23:09:16 +0200

    modified:   mordain.lua

--- mordain.lua
+++ mordain.lua
@@ -44,69 +44,68 @@ nssm:register_mob("nssm:mordain", {
 		punch_end = 225,
 	},
 	custom_attack = function(self)
-			if self.timer > 1 then
-				self.timer = 0
-				local s = self.object:getpos()
-				local p = self.attack:getpos()
+		if self.timer > 1 then
+			self.timer = 0
+			local s = self.object:getpos()
+			local p = self.attack:getpos()
 
-				if minetest.line_of_sight({x = p.x, y = p.y +1.5, z = p.z}, {x = s.x, y = s.y +1.5, z = s.z}) == true then
-					-- play attack sound
-					if self.sounds.attack then
-						minetest.sound_play(self.sounds.attack, {
-							object = self.object,
-							max_hear_distance = self.sounds.distance
-						})
+			if minetest.line_of_sight({x = p.x, y = p.y +1.5, z = p.z}, {x = s.x, y = s.y +1.5, z = s.z}) == true then
+				-- play attack sound
+				if self.sounds.attack then
+					minetest.sound_play(self.sounds.attack, {
+						object = self.object,
+						max_hear_distance = self.sounds.distance
+					})
+				end
+				-- punch player
+				self.attack:punch(self.object, 1.0,  {
+					full_punch_interval=1.0,
+					damage_groups = {fleshy=self.damage}
+				}, nil)
+			end
+
+			minetest.after(1, function()
+				local ty = s.y
+				local flag = 0
+				local m = 3
+
+				local v = {x=(p.x-s.x)*m, y = ty, z = (p.z-s.z)*m}
+				local d = {x=s.x+v.x, y = ty, z = s.z+v.z}
+
+				d.y = ty
+
+				for j = -3,3 do
+					ty = d.y + j
+					local current = minetest.env:get_node({x = d.x, y = ty, z = d.z}).name
+					local up = minetest.env:get_node({x = d.x, y = ty+1, z = d.z}).name
+					if up == "air" and current ~= "air" then
+						d.y = d.y + j+1.5
+						flag = 1
+						break
 					end
-					-- punch player
-					self.attack:punch(self.object, 1.0,  {
-						full_punch_interval=1.0,
-						damage_groups = {fleshy=self.damage}
-					}, nil)
 				end
 
-				minetest.after(1, function()
-					local ty = s.y
-					local flag = 0
-					local m = 3
-
-					local v = {x=(p.x-s.x)*m, y = ty, z = (p.z-s.z)*m}
-					local d = {x=s.x+v.x, y = ty, z = s.z+v.z}
-
-					d.y = ty
-
-					for j = -3,3 do
-						ty = d.y + j
-						local current = minetest.env:get_node({x = d.x, y = ty, z = d.z}).name
-						local up = minetest.env:get_node({x = d.x, y = ty+1, z = d.z}).name
-						if up == "air" and current ~= "air" then
-							d.y = d.y + j+1.5
-							flag = 1
-							break
-						end
-					end
-
-					while flag ~= 1 do
-						d.x = p.x + math.random(-m,m)
-						d.z = p.z + math.random(-m,m)
-						d.y = p.y
-						local dist = nssm:dist_pos(d, p)
-						if dist>=2 then
-							for j = -3,3 do
-								ty = d.y + j
-								local current = minetest.env:get_node({x = d.x, y = ty, z = d.z}).name
-								local up = minetest.env:get_node({x = d.x, y = ty+1, z = d.z}).name
-								if up == "air" and current ~= "air" then
-									d.y = d.y + j+1.5
-									flag = 1
-									break
-								end
+				while flag ~= 1 do
+					d.x = p.x + math.random(-m,m)
+					d.z = p.z + math.random(-m,m)
+					d.y = p.y
+					local dist = nssm:dist_pos(d, p)
+					if dist>=2 then
+						for j = -3,3 do
+							ty = d.y + j
+							local current = minetest.env:get_node({x = d.x, y = ty, z = d.z}).name
+							local up = minetest.env:get_node({x = d.x, y = ty+1, z = d.z}).name
+							if up == "air" and current ~= "air" then
+								d.y = d.y + j+1.5
+								flag = 1
+								break
 							end
 						end
 					end
-					self.object:setpos(d)
-					self.timer = 2
-				end)
-			end
-
+				end
+				self.object:setpos(d)
+				self.timer = 2
+			end)
+		end
 	end
 })
