c9a75b980e33159b39dda3d3ee3652bdf568c70e
NPXcoot <npx_npx@yahoo.it>
2017-01-16 23:07:52 +0100

    New mobs

---
+++ mobs/river_lord.lua
@@ -0,0 +1,58 @@
+mobs:register_mob("nssm:river_lord", {
+	type = "monster",
+	hp_max = 36,
+	hp_min = 27,
+	collisionbox = {-0.6, 0, -0.6, 0.6, 3, 0.6},
+	visual = "mesh",
+	mesh = "river_lord.x",
+	textures = {{"river_lord.png"}},
+	visual_size = {x=4, y=4},
+	makes_footstep_sound = true,
+	view_range = 10,
+	walk_velocity = 0.6,
+	rotate = 270,
+	fear_height = 4,
+	run_velocity = 4,
+	--[[sounds = {
+		random = "river_lord",
+	},]]
+	damage = 1,
+	jump = true,
+	drops = {
+		{name = "nssm:life_energy",
+		chance = 1,
+		min = 3,
+		max = 4},
+    },
+	armor = 70,
+	drawtype = "front",
+	water_damage = 0,
+	lava_damage = 5,
+	reach= 4,
+	light_damage = 0,
+	--group_attack=true,
+	--attack_animals=true,
+	knock_back=0,
+	blood_texture="nssm_blood.png",
+	stepheight=2.1,
+	on_rightclick = nil,
+	attack_type = "dogfight",
+	animation = {
+		speed_normal = 20,
+		speed_run = 20,
+		stand_start = 20,
+		stand_end = 100,
+		walk_start = 110,
+		walk_end = 150,
+		run_start = 160,
+		run_end = 180,
+		punch_start = 260,
+		punch_end = 280,
+		die_start = 290,
+		die_end = 310,
+		--Arena di fango inizio: 190
+		--Arena di fango fine: 220
+		--Inizio carica 230
+		--Fine carica 250
+    },
+})
